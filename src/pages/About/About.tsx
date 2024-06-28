5ac295fc09b285dc9a3bb1f68c82496e3af1a0df
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-06-28 17:42:55 -0300

    textos

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -12,7 +12,7 @@ const About: React.FC = () => {
       <div className="about-content">
         <h1 className="about-title">Sobre</h1>
         <p className="about-description">
-          A JC Goleiros é mais do que um centro de treinamento; é um espaço onde o amor pelo futebol se encontra com a disciplina e a técnica. Desde [ano de fundação], temos nutrido talentos, proporcionando não apenas habilidades técnicas robustas, mas também valores como trabalho em equipe, resiliência e dedicação ao esporte.
+          A JC Goleiros é mais do que uma escolinha; é onde o amor pelo futebol se encontra com a disciplina e a técnica. Desde 2020, temos nutrido talentos, proporcionando não apenas habilidades técnicas robustas, mas também valores como trabalho em equipe, resiliência e dedicação ao esporte.
         </p>
         <p className="about-description2"></p>
         <p className="about-description2"></p>
